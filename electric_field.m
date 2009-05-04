6015647ecd67179e7d19f65381569cec774e1444
salinasv <salinasv@gmail.com>
2009-05-04 01:16:45 -0500

    Add a function that calculates the electric field on the circuit.

---
+++ electric_field.m
@@ -0,0 +1,28 @@
+function [E,V] = electric_field(Q, CIRC, CELL_MM)
+%function [E,V] = electric_field(Q, CIRC, CELL_MM)
+%
+% Calculates the Electric field matrix based on the Q matrix.
+%
+% Return:
+% E: 	Electric Field magnitude on each point.
+% V: 	Electric Field direction on each point.
+% 
+% Params:
+% Q: 		The charge matrix.
+% CIRC: 	Cirquit area matrix.
+% CELL_MM: 	Number of computational cells per mm.
+
+siz = size(Q);
+K_e = 9e9;
+
+% Iterate over the entire matrix
+for x = 1:siz(1);
+	for y = 1:siz(2);
+		
+		[res, angle] = sum_qi_ri(Q, CIRC, x, y, CELL_MM);
+		E(x,y) = K_e .* res;
+		V(x,y) = angle;
+	end
+end
+
+
