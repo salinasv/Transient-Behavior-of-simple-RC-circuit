12d758f8d065a99a8c10446c4c114ebac00ac475
salinasv <salinasv@gmail.com>
2009-05-13 10:33:29 -0500

    Move charges between cells.

--- d_charge.m
+++ d_charge.m
@@ -1,4 +1,4 @@
-function Dq = d_charge(Q, CIRC, sigma, Et, THt, s_2, dt)
+function Dq = d_charge(Q, CIRC, sigma, Ex, Ey, s_2, dt)
 % function Dq = d_charge(omega, E, s, dt)
 %
 % Define how carges are moved across the face from cell to cell
@@ -16,9 +16,36 @@ siz = size(Q);
 for x = 1:siz(1)  
 	for y = 1:siz(2)
 
+		in = 0;
+
 		% Validate if we are on a "conductive cell"    
-		if CIRC(x,y)
-			Dq(x,y) = sigma .* Et(x,y) .* cos(THt(x,y)) .* s_2 .* dt;
+		if CIRC(x,y) && Q(x,y) ~= 0
+			%Dq(x,y) = sigma .* Et(x,y) .* cos(THt(x,y)) .* s_2 .* dt;
+			% Get the charge that moves througth y axis
+			tmp_y = Ey(x,y) .* sigma .* s_2 .* dt;
+			% Same for x axis
+			tmp_x = Ex(x,y) .* sigma .* s_2 .* dt;
+
+			% We get the charge from our neigbors
+			if CIRC(x,y-1)
+				Dq(x,y-1) = -tmp_y;
+				in = tmp_y;
+			end
+			if CIRC(x+1,y)
+				Dq(x+1,y) = -tmp_x;
+				in = in + tmp_x;
+			end
+			if CIRC(x,y+1)
+				Dq(x,y+1) = -tmp_y;
+				in = in + tmp_y;
+			end
+			if CIRC(x-1,y)
+				Dq(x-1,y) = -tmp_x;
+				in = in + tmp_x;
+			end
+
+			% and we get all this borrowed charge for ourself
+			Dq(x,y) = in;
 		else
 			Dq(x,y) = 0;
 		end
