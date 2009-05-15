c76dff0b20eb73262c4ab28b8c5be0877970f4e4
salinasv <salinasv@gmail.com>
2009-05-15 01:37:57 -0500

    Use sigma values to define the area.

--- create_area.m
+++ create_area.m
@@ -10,11 +10,13 @@ function [Area, Q] = create_area(CELL_MM, CAP_WIDTH, CAP_HEIGTH, WIRE_WIDTH);
 
 % Defineh initial charge
 CHARGE = 4e5;
+S_COPPER = 1 ./ 1.7e-8;
+S_GLASS = 1 ./ 10e14;
 
 % define work area
 lengthA = int32(CAP_HEIGTH*2 * CELL_MM);
 Area(lengthA,lengthA) = 0;
-Q(lengthA,lengthA) = 0;
+Q(lengthA,lengthA) = S_GLASS;
 % draw the capacitor in the center
 center = lengthA / 2;
 width_cell = CAP_WIDTH * CELL_MM;
@@ -26,11 +28,11 @@ cap1_left_limit = int32(center - lengthA(1)*0.05 - width_cell);
 cap1_right_limit = int32(center - lengthA(1)*0.05);
 cap_up_limit = int32(lengthA*0.1);
 cap_down_limit = int32(lengthA*0.1 + heigth_cell);
-Area( cap1_left_limit:cap1_right_limit , cap_up_limit:cap_down_limit  ) = 1;
+Area( cap1_left_limit:cap1_right_limit , cap_up_limit:cap_down_limit  ) = S_COPPER;
 	%right side
 cap2_right_limit = int32(center + lengthA(1)*0.05 + width_cell);
 cap2_left_limit = int32(center + lengthA(1)*0.05);
-Area( cap2_left_limit:cap2_right_limit , cap_up_limit:cap_down_limit  ) = 1;
+Area( cap2_left_limit:cap2_right_limit , cap_up_limit:cap_down_limit  ) = S_COPPER;
 % Charge capacitor
 % set a puntual charge at the center
 cap_center = int32((cap_down_limit - cap_up_limit) ./2 + cap_up_limit);
@@ -47,14 +49,14 @@ wire_left = int32(lengthA * 0.1);
 wire_right = int32(lengthA - lengthA * 0.1);
 wire_up = int32(wire_center - WIRE_WIDTH/2 * CELL_MM);
 wire_down = int32(wire_center + WIRE_WIDTH/2 * CELL_MM);
-Area( wire_left:cap1_left_limit, wire_up:wire_down) = 1;
-Area( cap2_right_limit:wire_right, wire_up:wire_down) = 1;
+Area( wire_left:cap1_left_limit, wire_up:wire_down) = S_COPPER;
+Area( cap2_right_limit:wire_right, wire_up:wire_down) = S_COPPER;
 	% bottom wire
 wire_b_up = int32(cap_down_limit + lengthA * 0.1);
 wire_b_down = wire_b_up + WIRE_WIDTH * CELL_MM;
-Area( wire_left:wire_right, wire_b_up: wire_b_down) = 1;
+Area( wire_left:wire_right, wire_b_up: wire_b_down) = S_COPPER;
 	% up-down wires
 wire_ud_rigth = wire_left + WIRE_WIDTH * CELL_MM;
 wire_ud_left = wire_right - WIRE_WIDTH * CELL_MM;
-Area( wire_left:wire_ud_rigth, wire_up:wire_b_down) = 1;
-Area( wire_ud_left:wire_right, wire_up:wire_b_down) = 1;
+Area( wire_left:wire_ud_rigth, wire_up:wire_b_down) = S_COPPER;
+Area( wire_ud_left:wire_right, wire_up:wire_b_down) = S_COPPER;
